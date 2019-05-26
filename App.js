e69a778d627828485b4e7419e3bc873cb718574a
SSCastanedaC <sscastanedac@gmail.com>
2019-05-25 20:35:45 -0500

    fix Add Import Image Component from RN

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {ScrollView, StyleSheet, Text, View, Dimensions} from 'react-native';
+import {ScrollView, StyleSheet, Text, View, Dimensions, Image} from 'react-native';
 
 var {height, width} = Dimensions.get('window');
 
